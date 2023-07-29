a0f6d0cdb897d659ce30d386371ca4a1f9dbac8d
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2023-07-30 01:11:14 +0200

    new update: bug fix

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ import os
 import json
 from flask import Flask, render_template, request, redirect, url_for
 from flask import send_from_directory
-from helpers import Member
+from helpers import Member, CustomJSONEncoder
 from flask import jsonify
 from datetime import datetime
 
@@ -15,7 +15,7 @@ members_file_path = os.path.join(BASE_DIR, 'data', 'members.json')
 with open(members_file_path) as f:
     members_data = json.load(f)
 
-members = [Member(data['name'], data['contributions']) for data in members_data]
+members = [Member(data['name'], data.get('adhesion_date'), data['contributions']) for data in members_data]
 
 
 @app.route('/')
@@ -27,14 +27,63 @@ def home():
 def add_member():
     if request.method == 'POST':
         name = request.form.get('name')
-        new_member = Member(name, [])
+        new_member = Member(name, datetime.now().strftime('%d/%m/%Y %H:%M:%S'), [])
         members.append(new_member)
         # Also add the new member to the JSON file
         with open(members_file_path, 'w') as f:
-            members_data.append({'name': name, 'contributions': []})
+            members_data.append({
+                'name': name,
+                'adhesion_date': datetime.now().strftime('%d/%m/%Y %H:%M:%S'),
+                'contributions': []})
             json.dump(members_data, f)
         return redirect(url_for('home'))
     return render_template('add_member.html')
+
+
+@app.route('/update_member/<string:name>', methods=['GET', 'POST'])
+def update_member(name):
+    if request.method == 'POST':
+        new_name = request.form['new_name']
+        for member in members:
+            if member.name == name:
+                member.name = new_name
+
+        # Persist the changes to the JSON file
+        with open(members_file_path, 'w') as f:
+            members_data = [{
+                'name': member.name,
+                'adhesion_date': member.adhesion_date.strftime('%Y-%m-%d %H:%M:%S'),
+                'contributions': [{
+                    'date': contribution['date'].strftime('%Y-%m-%d %H:%M:%S'),
+                    'amount': contribution['amount']
+                } for contribution in member.contributions]
+            } for member in members]
+            json.dump(members_data, f)
+
+        return redirect(url_for('overview'))
+    return render_template('update_member.html', name=name)
+
+
+from flask import jsonify, make_response
+
+
+@app.route('/delete_member/<string:name>', methods=['GET', 'POST'])
+def delete_member(name):
+    global members_data
+    member = next((item for item in members_data if item["name"] == name), None)
+    if member:
+        members_data.remove(member)
+        with open(members_file_path, 'w') as f:
+            # Use a custom JSONEncoder to handle datetime objects
+            json.dump(members_data, f, default=CustomJSONEncoder().default)
+        return make_response(jsonify({"message": f'Member {name} deleted successfully'}), 200)
+    else:
+        return make_response(jsonify({"error": "Member not found"}), 404)
+
+
+@app.route('/overview')
+def overview():
+    return render_template('overview.html', members=members)
 
 
 @app.route('/export_data')
@@ -59,7 +108,8 @@ def import_data():
         if file and file.filename.endswith('.json'):
             data_to_import = json.load(file)
             global members
-            members = [Member(data['name'], data['contributions']) for data in data_to_import]
+            members = [Member(data['name'], data.get('adhesion_date'), data['contributions']) for data in
+                       data_to_import]
             with open(members_file_path, 'w') as f:
                 json.dump(data_to_import, f)
             return redirect(url_for('home'))
@@ -74,6 +124,11 @@ def add_contribution(name):
         for member in members:
             if member.name == name:
                 member.add_contribution(float(amount), date)
+
+        # After updating the members, write the changes back to the JSON file
+        with open(members_file_path, 'w') as f:
+            json.dump([member.to_dict() for member in members], f)
+
         return redirect(url_for('home'))
     return render_template('add_contribution.html', name=name)
 
@@ -84,7 +139,7 @@ def get_total_contributions_by_month(year, month):
     for member in members:
         total = 0
         for contribution in member.contributions:
-            contribution_date = datetime.strptime(contribution['date'], '%d/%m/%Y %H:%M:%S')
+            contribution_date = contribution['date']
             if contribution_date.year == year and contribution_date.month == month:
                 total += contribution['amount']
         total_contributions.append({'name': member.name, 'total': total})
--- app/helpers.py
+++ app/helpers.py
@@ -1,23 +1,49 @@
 from datetime import datetime
+from flask.json import JSONEncoder
 
 
 def _convert_date_format(date_str):
-    try:
-        # Try to parse the date with the new format
-        return datetime.strptime(date_str, '%d/%m/%Y %H:%M:%S')
-    except ValueError:
-        # If it fails, try with the old format
-        return datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
+    formats = ["%Y-%m-%dT%H:%M:%S.%f", "%Y-%m-%dT%H:%M:%S", "%d/%m/%Y %H:%M:%S", "%d/%m/%Y", "%Y-%m-%d %H:%M:%S"]
+    for date_format in formats:
+        try:
+            return datetime.strptime(date_str, date_format)
+        except ValueError:
+            pass
+    raise ValueError(f"time data {date_str} does not match any of the expected formats")
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, datetime.datetime):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
 
 
 class Member:
-    def __init__(self, name, contributions=None):
+    def __init__(self, name, adhesion_date=None, contributions=None):
         self.name = name
+        if adhesion_date is None:
+            self.adhesion_date = datetime.now()
+        else:
+            self.adhesion_date = _convert_date_format(adhesion_date)
         if contributions:
             self.contributions = [{'amount': c['amount'], 'date': _convert_date_format(c['date'])} for c in
                                   contributions]
         else:
             self.contributions = []
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'adhesion_date': self.adhesion_date.isoformat(),
+            'contributions': [{'amount': c['amount'], 'date': c['date'].isoformat()} for c in self.contributions]
+        }
 
     @property
     def total_contributions(self):
@@ -29,4 +55,3 @@ class Member:
         if isinstance(date, str):
             date = datetime.strptime(date, '%d/%m/%Y %H:%M:%S')
         self.contributions.append({'amount': amount, 'date': date})
-
